900dc3c4bbaf9a4de7ce2fc1a4ddba8f8f527720
senun <marcelimnunes@gmail.com>
2026-06-26 16:56:32 -0300

    Fix: Update next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -5,13 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config, { isServer }) => {
-    return config;
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
